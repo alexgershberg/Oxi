a865297a7ad89d12a3d89aab35a1b66ca24372a6
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2022-09-22 18:34:29 +0100

    Add error handling

--- Client/view.py
+++ Client/view.py
@@ -19,7 +19,12 @@ def broadcast(response_json: T_Response_Json, local_index: int, db_index: int) -
     records = response_json["records"]
 
     for message_index in range(local_index + 1, db_index + 1):
-        current_record: T_Yell_Json = ast.literal_eval(records[str(message_index)])
+        try:
+            current_record: T_Yell_Json = ast.literal_eval(records[str(message_index)])
+        except KeyError:
+            print(f'[ERROR] KeyError: {message_index}')
+            print(f'{records}')
+
         # Yell_Json keys get encoded into bytes, when we store things into the redis DB.
         # ("time" becomes b"time")
         # Don't know how to get around that just yet
@@ -39,7 +44,7 @@ if __name__ == "__main__":
             "max_load": f"{max_load}"  # Load at most N amount of messages
         }
 
-        time.sleep(7)
+        time.sleep(1)
 
         raw_response_json = None
         try:
@@ -49,6 +54,7 @@ if __name__ == "__main__":
 
         if raw_response_json is not None:
             response_json: T_Response_Json = ast.literal_eval(raw_response_json.text)
+            #print(f'[DEBUG] {response_json}')
             message_count: int = int(response_json["message_count"])
             db_index: int = int(response_json["db_index"])
             start_index: int = db_index - message_count
